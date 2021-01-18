36007f6eda765069a06e9f94a91477a011ef244b
damung18 <77581632+damung18@users.noreply.github.com>
2021-01-18 14:02:20 +0700

    Add files via upload

---
+++ jajal.sh
@@ -0,0 +1,2 @@
+defps1="PS1='\\\[\\\e[39m\\\]"ND-damung"\\\[\\\e[92m\\\]pandol\\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[92m\\\]'"
+sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
